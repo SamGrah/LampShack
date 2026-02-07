63b7238b8dbf871800424bd0ff2cb27efee20079
SamGrah <segrah@gmail.com>
2026-02-06 20:57:17 -0500

    gallery image enlargement, robots.txt, and sitemap xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.thelampshack.com',
   adapter: netlify(),
+  integrations: [sitemap()],
   prefetch: {
     prefetchAll: true
   },
